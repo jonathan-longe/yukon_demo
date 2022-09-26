681bc7aa5d8b57ffe5266ff10892ceae185eef34
Jonathan Longe <jlonge@corebox.net>
2022-09-25 22:33:56 -0700

    add viewer to docker-compose.yml

--- python/listener/config.py
+++ python/listener/config.py
@@ -4,6 +4,6 @@ from python.common.config import Config as BaseConfig
 
 class Config(BaseConfig):
     # See:  https://www.rabbitmq.com/streams.html
-    X_STREAM_OFFSET                         = os.getenv('X_STREAM_OFFSET', 'next')
+    X_STREAM_OFFSET                         = os.getenv('X_STREAM_OFFSET')
     CALLBACK_FN_NAME                        = os.getenv('CALLBACK_FN_NAME', 'write_to_database')
     DATABASE_API_URI                        = os.getenv('DATABASE_API_URI', 'http://database_api:5000/events')
